42a72cd4783ff169eb02500fe398bb8b103b3e59
Niklas Schachl <niklas.schachl@htl-wels.at>
2021-05-27 10:34:48 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+//********************************************************************
+//
+// Program        : Übung 25 - Schuelerverwaltung
+//
+// Author         : Niklas Schachl
+//
+// Date           : 27.5.2021
+//
+// Description    : Program to manage students
+//
+//********************************************************************
+
+//define struct
+typedef struct
+{
+    int id_number;
+    char name[32];
+    int grade;
+}
+student;
+
+int main()
+{
+
+
+    return 0;
+}
+
+void create()
+{
+
+}
